bd23006c2d3071dbdd7ceb5d1e8d2c47ed4a14ef
ImranSerifovic <116844006+ImranSerifovic@users.noreply.github.com>
2023-09-07 18:42:09 -0400

    yup

--- TestCode0904/src/robot-config.cpp
+++ TestCode0904/src/robot-config.cpp
@@ -10,12 +10,12 @@ brain  Brain;
 // VEXcode device constructors
 motor Right1 = motor(PORT1, ratio6_1, false);
 motor Right2 = motor(PORT2, ratio6_1, false);
-motor Left1 = motor(PORT12, ratio6_1, false);
-motor Left2 = motor(PORT13, ratio6_1, false);
+motor Left1 = motor(PORT12, ratio6_1, true);
+motor Left2 = motor(PORT13, ratio6_1, true);
 inertial Inertial21 = inertial(PORT21);
 controller Controller1 = controller(primary);
 motor Cata = motor(PORT3, ratio36_1, false);
-motor Intake = motor(PORT19, ratio6_1, false);
+motor Intake = motor(PORT19, ratio6_1, true);
 
 // VEXcode generated functions
 // define variable for remote controller enable/disable
