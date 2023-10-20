6e53560082ab106eeed98e9b209b4b9e950c0ea9
ImranSerifovic <116844006+ImranSerifovic@users.noreply.github.com>
2023-10-20 13:12:14 -0400

    more skills innit

--- TestCode0904/src/autons.cpp
+++ TestCode0904/src/autons.cpp
@@ -4,7 +4,7 @@ void default_constants(){
   
   chassis.set_heading_constants(6, .4, 0, 1, 0);
   chassis.set_swing_constants(12, .3, .001, 2, 15);
-  chassis.set_drive_exit_conditions(1.5, 300, 5000);
+  chassis.set_drive_exit_conditions(2.5, 300, 5000);
   chassis.set_turn_exit_conditions(2, 300, 3000);
   chassis.set_swing_exit_conditions(1, 300, 3000);
 
@@ -49,14 +49,14 @@ void skills(){
   drive(7, true);
   wait(0.4, seconds);
   drive_stop();
-  for(int i = 0; i<3; i++) {
-    Cata.setVelocity(30, percent); 
-    Cata.spinFor(forward, 75.0, degrees);
-    Cata.setVelocity(100, percent); 
-    Cata.spinFor(forward, 105.0, degrees);
-  }
+  // for(int i = 0; i<3; i++) {
+  //   Cata.setVelocity(30, percent); 
+  //   Cata.spinFor(forward, 75.0, degrees);
+  //   Cata.setVelocity(100, percent); 
+  //   Cata.spinFor(forward, 105.0, degrees);
+  // }
   flaps.set(true);
-  chassis.drive_distance(-20, -135);
+  chassis.drive_distance(-20, -127);
   chassis.drive_distance(-60);
   setVelocity(100);
   drive(12, false);
@@ -64,9 +64,9 @@ void skills(){
   drive_stop();
   chassis.drive_distance(67, 180);
   chassis.drive_distance(210, 188);
-  chassis.drive_distance(50, 172);
-  chassis.drive_distance(20, 60);
-  chassis.drive_distance(100);
+  chassis.drive_distance(40, 172);
+  chassis.drive_distance(120, 75);
+  // chassis.drive_distance(120);
   chassis.drive_distance(50, 170);
   flaps.set(true);
   chassis.drive_distance(20, 170);
